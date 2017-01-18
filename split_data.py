c0dc544cf85fc415c99c4cfddee1317487065c4d
Simon Bedford <simon.bedford@gmail.com>
2017-01-18 08:27:57 -0600

    initial commit for ecobici model training code

---
+++ split_data.py
@@ -0,0 +1,34 @@
+"""
+Create all features and split the data into training and test sets.
+"""
+
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+import sys
+
+
+if __name__ == "__main__":
+
+    num_rows = int(sys.argv[1])
+    if num_rows == -1:
+        num_rows = None
+
+    all_data = pd.read_csv('data/model_data.csv', nrows=num_rows)
+
+    # Need these because did not filter correctly in SQL query and to avoid
+    # outliers and data errors
+    all_data = all_data[all_data['age'] <= 85]
+    all_data = all_data[all_data['hexagon_id'].notnull()]
+
+    all_data['age_bucket'] = pd.cut(all_data['age'], bins=(
+        16, 20, 30, 40, 50, 60, 70, 80, 85), include_lowest=True, retbins=False)
+    all_data = all_data.drop('age', axis=1)
+    all_data = all_data.drop('id', axis=1)
+
+    all_data = pd.get_dummies(
+        all_data, columns=['start_hour', 'month', 'hexagon_id', 'age_bucket'])
+
+    train, test = train_test_split(all_data, test_size=0.2, random_state=0)
+
+    train.to_csv('output/train_data.csv', index=None)
+    test.to_csv('output/test_data.csv', index=None)
---
+++ tune_params.py
@@ -0,0 +1,65 @@
+"""
+Train a machine learning model on Ecobici trips data for trips between 2014-01-01 AND 2016-07-31
+
+The aim is to predict bicycle trip duration in seconds based on the following features:
+
+- Gender
+- Age
+- Weekday vs. Weekend
+- Hour of Day
+- Month
+- Start location based on hexagonal grid
+
+"""
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import r2_score, median_absolute_error, mean_squared_error, mean_absolute_error
+import logging
+
+
+def regression_report(y_true, y_pred):
+    report = ""
+    report += "R2 Score:\t\t{}".format(r2_score(y_true, y_pred))
+    report += "\n\n"
+    report += "median_absolute_error:\t\t{}".format(median_absolute_error(y_true, y_pred))
+    report += "\n\n"
+    report += "mean_squared_error:\t\t{}".format(mean_squared_error(y_true, y_pred))
+    report += "\n\n"
+    report += "mean_absolute_error:\t\t{}".format(mean_absolute_error(y_true, y_pred))
+    report += "\n\n"
+    return report
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="output/paramater_tuning.log", level=logging.INFO)
+
+    cols = pd.read_csv('output/train_data.csv', nrows=1).columns.tolist()
+    cols.remove('duration')
+
+    X_train = pd.read_csv('output/train_data.csv', usecols=cols)
+    y_train = np.ravel(pd.read_csv(
+        'output/train_data.csv', usecols=['duration']))
+
+    params = {
+        'n_estimators': [10, 100, 250, 500, 1000],
+        'max_features': ['auto', 'sqrt', 0.5],
+        'min_samples_leaf': [1, 10, 50]
+    }
+
+    clf = GridSearchCV(RandomForestRegressor(random_state=1), params, cv=5, n_jobs=-1)
+    clf.fit(X_train, y_train)
+
+    logging.info("Best parameters set found on training set:\n\n{}\n".format(clf.best_params_))
+
+    X_test = pd.read_csv('output/test_data.csv', usecols=cols)
+    y_test = np.ravel(pd.read_csv(
+        'output/test_data.csv', usecols=['duration']))
+
+    logging.info("Detailed classification report:\n")
+    y_true, y_pred = y_test, clf.predict(X_test)
+    logging.info(regression_report(y_true, y_pred))
+
+    np.save("output/predictions.npy", y_pred)
