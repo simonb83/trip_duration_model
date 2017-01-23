a00abf4741a80225a90f4451057d2ec041057bb3
Simon Bedford <simon.bedford@gmail.com>
2017-01-22 19:00:27 -0600

    fit random forest model

--- fit_classifier_model.py
+++ fit_classifier_model.py
@@ -14,6 +14,7 @@ The aim is to predict bicycle trip duration in seconds based on the following fe
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.externals import joblib
 import logging
@@ -21,90 +22,78 @@ import argparse
 
 
 def duration_classes(x):
-    if x <= 120:
+    if x <= 60 * 15:
         return 1
-    elif x <= 300:
+    if x <= 60 * 30:
         return 2
-    elif x <= 600:
+    if x <= 60 * 45:
         return 3
-    elif x <= 900:
+    if x <= 60 * 60:
         return 4
-    elif x <= 1200:
-        return 5
-    elif x <= 1500:
-        return 6
-    elif x <= 1800:
-        return 7
-    elif x <= 2700:
-        return 8
-    else:
-        return 9
+
+
+def pre_process_data(data):
+    data['duration'] = data['duration'].apply(lambda x: duration_classes(x))
+    data.reset_index(inplace=True, drop=True)
+    return data
+
+
+def parse_data(data):
+    y = np.ravel(data['duration'])
+    data = data.drop('duration', axis=1)
+    return data, y
 
 if __name__ == "__main__":
 
-    logging.basicConfig(filename="output/fit_classifier_model.log", level=logging.INFO)
+    logging.basicConfig(
+        filename="output/fit_classifier_model.log", level=logging.INFO)
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-l", "--loss", help="Loss function")
+        "-n", "--estimators", help="Number of estimators")
     parser.add_argument(
-        "-p", "--penalty", help="Penalty")
+        "-f", "--features", help="Max features")
     parser.add_argument(
-        "-e", "--eta", help="Eta0")
+        "-s", "--sample", help="Training sample size")
     parser.add_argument(
-        "-c", "--chunksize", help="Chunksize for processing training data")
+        "-t", "--test", help="Test sample size")
     args = parser.parse_args()
 
-    loss = str(args.loss)
-    penalty = str(args.penalty)
-    eta = float(args.eta)
-    chunksize = int(args.chunksize)
+    n_estimators = int(args.estimators)
+    features = str(args.features)
+    train_sample = int(args.sample)
+    test_sample = int(args.test)
 
-    epsilon = 60
-    num_iterations = 1
-    random_state = 1
+    class_weights = {1: 0.708, 2: 0.237, 3: 0.049, 4: 0.006}
 
-    all_classes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    class_weights = {
-        1: 0.01, 2: 0.14, 3: 0.34, 4: 0.22, 5: 0.12,
-        6: 0.07, 7: 0.04, 8: 0.05, 9: 0.01}
+    clf = RandomForestClassifier(
+        n_estimators=n_estimators, max_features=features, class_weight=class_weights)
 
-    clf = SGDClassifier(
-        loss=loss, penalty=penalty, n_iter=num_iterations, epsilon=epsilon, random_state=random_state, eta0=eta, class_weight=class_weights)
+    store = pd.HDFStore('output/train.h5')
+    nrows = store.get_storer('train').nrows
+    r = np.random.randint(0, nrows, size=train_sample)
+    data = pd.read_hdf('output/train.h5', 'train', where=pd.Index(r))
+    data = pre_process_data(data)
 
-    # Iterate over the training data
-    i = 1
-    for chunk in pd.read_hdf('output/train.h5', chunksize=chunksize):
-        logging.info("Processing chunk {}".format(i))
-        chunk = chunk.drop('Unnamed: 0', axis=1)
-        cols = chunk.columns.tolist()
-        cols.remove('duration')
-        chunk['duration'] = chunk['duration'].apply(lambda x: duration_classes(x))
-        y = np.ravel(chunk['duration'])
-        chunk = chunk.drop('duration', axis=1)
-        clf.partial_fit(chunk, y, classes=all_classes)
-        i += 1
+    train_X, train_y = parse_data(train_data)
+    clf.fit(train_X, train_y)
 
-    logging.info("Starting test run")
-    i = 1
-    # Make some predictions also in chunks
-    for chunk in pd.read_hdf('output/test.h5', chunksize=chunksize):
-        logging.info("Processing chunk {}".format(i))
-        chunk = chunk.drop('Unnamed: 0', axis=1)
-        cols = chunk.columns.tolist()
-        cols.remove('duration')
-        chunk['duration'] = chunk['duration'].apply(lambda x: duration_classes(x))
-        y = np.ravel(chunk['duration'])
-        chunk = chunk.drop('duration', axis=1)
+    store = pd.HDFStore('output/test.h5')
+    nrows = store.get_storer('test').nrows
+    r = np.random.randint(0, nrows, size=test_sample)
+    data = pd.read_hdf('output/test.h5', 'test', where=pd.Index(r))
+    data = pre_process_data(data)
 
-        y_pred = clf.predict(chunk)
-        df = pd.DataFrame(np.array([y, y_pred]).T,
-                          columns=['True', 'Predicted'])
-        df.to_hdf('output/predicted_class.h5', 'predicted_class', append=True, format='t')
-        i += 1
+    test_X, test_y = parse_data(train_data)
+    clf.fit(train_X, train_y)
+    y_pred = clf.predict(test_X)
 
-    logging.info("Save model to disk:\n")
-    joblib.dump(clf, 'models/classifier/classifier.pkl')
+    df = pd.DataFrame(np.array([test_y, y_pred]).T,
+                      columns=['True', 'Predicted'])
+    df.to_hdf('output/predicted_class.h5',
+              'predicted_class', append=True, format='t')
+
     logging.info("Detailed classification report:\n")
     df = pd.read_hdf('output/predicted_class.h5')
-    logging.info(classification_report(df['True'], df['Predicted']), target_names=all_classes)
+    logging.info(classification_report(df['True'], df[
+                 'Predicted']), target_names=all_classes)
